93b6b44045723ec5284d478405f95fb20251a587
roc102 <kyroc102@gmail.com>
2023-12-25 22:53:53 +0530

    adding file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from "react";
-import Header from './components/Header/Header.jsx';
 
 function App() {
   return (
     <div className="App">
-       <Header/>
      <section className="py-20 dark:bg-dark lg:py-[120px]">
      
         <h1 className="text-center mx-10">Appscrip</h1>
